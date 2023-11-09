9e423603014286b9c649b20696d6c222d6320981
junbok97 <junbok97@gmail.com>
2023-11-09 10:28:51 +0900

    feat: HeatPickFont 추가

---
+++ client/Targets/DesignSystem/DesignKit/Sources/Font/UIFont+DesignSystem.swift
@@ -0,0 +1,28 @@
+//
+//  UIFont+DesignSystem.swift
+//  DesignKit
+//
+//  Created by 이준복 on 2023/11/09.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import UIKit
+
+public extension UIFont {
+    
+    static let largeR: UIFont = .systemFont(ofSize: 21, weight: .regular)
+    static let largeSB: UIFont = .systemFont(ofSize: 21, weight: .semibold)
+    static let largeB: UIFont = .systemFont(ofSize: 21, weight: .bold)
+    
+    static let bodyR: UIFont = .systemFont(ofSize: 17, weight: .regular)
+    static let bodySB: UIFont = .systemFont(ofSize: 17, weight: .semibold)
+    static let bodyB: UIFont = .systemFont(ofSize: 17, weight: .bold)
+    
+    static let captionR: UIFont = .systemFont(ofSize: 15, weight: .regular)
+    static let captionSB: UIFont = .systemFont(ofSize: 15, weight: .semibold)
+    static let captionB: UIFont = .systemFont(ofSize: 15, weight: .bold)
+    
+    static let smallR: UIFont = .systemFont(ofSize: 13, weight: .regular)
+    static let smallSB: UIFont = .systemFont(ofSize: 13, weight: .semibold)
+    static let smallB: UIFont = .systemFont(ofSize: 13, weight: .bold)
+}
