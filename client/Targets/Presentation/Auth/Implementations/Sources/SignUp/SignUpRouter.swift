273c97aab37484a2363ad09ec1eea44f50fc37e2
jungminlim <jungmin.lim.cs@gmail.com>
2023-11-09 17:51:30 +0900

    feat: SignUp 리블렛 추가

---
+++ client/Targets/Presentation/Auth/Implementations/Sources/SignUp/SignUpRouter.swift
@@ -0,0 +1,27 @@
+//
+//  SignUpRouter.swift
+//  AuthImplementations
+//
+//  Created by jungmin lim on 11/9/23.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import ModernRIBs
+
+protocol SignUpInteractable: Interactable {
+    var router: SignUpRouting? { get set }
+    var listener: SignUpListener? { get set }
+}
+
+protocol SignUpViewControllable: ViewControllable {
+    // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+}
+
+final class SignUpRouter: ViewableRouter<SignUpInteractable, SignUpViewControllable>, SignUpRouting {
+
+    // TODO: Constructor inject child builder protocols to allow building children.
+    override init(interactor: SignUpInteractable, viewController: SignUpViewControllable) {
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+}
