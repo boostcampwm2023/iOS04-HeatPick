e95093a83a0da6948007569bf2f3f4896f1384eb
junbok97 <junbok97@gmail.com>
2023-11-09 10:23:59 +0900

    feat: HeatPickColor 추가

---
+++ client/Targets/DesignSystem/DesignKit/Sources/Color/UIColor+DesignSystem.swift
@@ -0,0 +1,30 @@
+//
+//  UIColor+DesignSystem.swift
+//  DesignKit
+//
+//  Created by 이준복 on 2023/11/09.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import UIKit
+
+public extension UIColor {
+    static let hpBlack: UIColor = .init(resource: .hpBlack)
+    static let hpWhite: UIColor = .init(resource: .hpWhite)
+    
+    static let hpGray1: UIColor = .init(resource: .hpGray1)
+    static let hpGray2: UIColor = .init(resource: .hpGray2)
+    static let hpGray3: UIColor = .init(resource: .hpGray3)
+    static let hpGray4: UIColor = .init(resource: .hpGray4)
+    static let hpGray5: UIColor = .init(resource: .hpGray5)
+    
+    
+    static let hpRed1: UIColor = .init(resource: .hpRed1)
+    static let hpRed2: UIColor = .init(resource: .hpRed2)
+    static let hpRed3: UIColor = .init(resource: .hpRed3)
+    static let hpRed4: UIColor = .init(resource: .hpRed4)
+    static let hpRed5: UIColor = .init(resource: .hpRed5)
+    
+    static let hpBlue1: UIColor = .init(resource: .hpBlue1)
+    static let hpBlue2: UIColor = .init(resource: .hpBlue2)
+}
