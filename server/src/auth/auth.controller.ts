5f47db3d8a9996f743cc7951e9e88ee66de13d52
Geomgi <cgg7777@naver.com>
2023-11-09 16:29:19 +0900

    feat: swagger 적용

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,12 +2,23 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth.credential.dto';
 import { RegisterDto } from './dto/auth.resgister.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { User } from 'src/entities/user.entity';
 
+@ApiBearerAuth()
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({ status: 200, description: 'Access Token' })
   async signUp(@Body() registerDto: RegisterDto): Promise<string> {
     return this.authService.signUp(
       registerDto.OAuthToken,
@@ -15,6 +26,11 @@ export class AuthController {
     );
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Access Token',
+    type: User,
+  })
   @Post('signin')
   async signIn(@Body() authCredentialDto: AuthCredentialDto): Promise<string> {
     return await this.authService.signIn(authCredentialDto.OAuthToken);
--- server/src/auth/dto/auth.resgister.dto.ts
+++ server/src/auth/dto/auth.resgister.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger/dist';
+
 export class RegisterDto {
+  @ApiProperty({ example: 'dsfAVBS3423ASD53245', description: 'OAuth Token' })
   OAuthToken: string;
+
+  @ApiProperty({ example: 'boostcamp', description: 'user nickname' })
   username: string;
 }
