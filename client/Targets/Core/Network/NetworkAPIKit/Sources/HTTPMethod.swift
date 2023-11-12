ef91be909ec8c13f06476b0c1b81f40d3a848e5c
hogumachu <castle_jun@daum.net>
2023-11-12 17:17:41 +0900

    feat: HTTP Method 추가

---
+++ client/Targets/Core/Network/NetworkAPIKit/Sources/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  NetworkAPIKit
+//
+//  Created by 홍성준 on 11/10/23.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import Foundation
+
+public enum HTTPMethod: String, Encodable {
+    
+    case get = "GET"
+    case post = "POST"
+    
+}
