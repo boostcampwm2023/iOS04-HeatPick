cf2ed4d1f912ef39e6a290693030c56da40cd70b
cgg7777 <cgg7777@naver.com>
2023-11-14 13:07:01 +0900

    chore: jwt 토큰 암호화 키 env파일로 관리하도록 변경

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,12 +5,15 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserRepository } from 'src/user/user.repository';
 import { userProviders } from 'src/user/user.providers';
 import { DatabaseModule } from 'src/db/database.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     DatabaseModule,
     JwtModule.register({
-      secret: 'secret',
+      secret: process.env.SECRET_KEY,
       signOptions: {
         expiresIn: 60 * 60,
       },
