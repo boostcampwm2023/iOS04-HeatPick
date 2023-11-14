af528546e174e363cc598d17da54529309b3cbb8
jungminlim <jungmin.lim.cs@gmail.com>
2023-11-14 16:46:13 +0900

    feat: detail viewController 및 Demo appDelegate 추가

--- client/Targets/Presentation/Story/Demo/Sources/AppDelegate.swift
+++ client/Targets/Presentation/Story/Demo/Sources/AppDelegate.swift
@@ -1,15 +1,17 @@
 import UIKit
 
+import StoryImplementations
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
-    private let storyComponent: 
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .systemPink
+        let viewController = StoryDetailViewController()
+
         window.rootViewController = viewController
         window.makeKeyAndVisible()
         return true
