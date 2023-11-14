a53123433cbc99abfc81657a61a23c5baa445194
junbok97 <junbok97@gmail.com>
2023-11-14 11:04:00 +0900

    chore: App폴더 분리

--- client/Targets/App/Sources/App/AppDelegate.swift
+++ client/Targets/App/Sources/App/AppDelegate.swift
@@ -6,18 +6,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    private let appComponent: AppComponent = .init()
     private var launchRouter: LaunchRouting?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
         
-        let router = AppRootBuilder(dependency: appComponent).build()
+        let router = AppRootBuilder(dependency: AppComponent()).build()
         self.launchRouter = router
         
         launchRouter?.launch(from: window)
-        appComponent.naverLoginRepository.setup()
         return true
     }
     
