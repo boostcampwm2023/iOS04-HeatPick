5645fdff35c0f9912047565ecab9b347c530da1b
Geomgi <cgg7777@naver.com>
2023-11-15 15:02:43 +0900

    feat: story-user, profile-user cascading, lazy loading 설정

--- server/src/story/story.module.ts
+++ server/src/story/story.module.ts
@@ -4,10 +4,12 @@ import { storyProvider } from 'src/story/story.providers';
 import { StoryService } from './story.service';
 import { StoryRepository } from './story.repository';
 import { StoryController } from './story.controller';
+import { UserRepository } from 'src/user/user.repository';
+import { userProviders } from 'src/user/user.providers';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [StoryController],
-  providers: [...storyProvider, StoryService, StoryRepository],
+  providers: [...storyProvider, ...userProviders, StoryService, StoryRepository, UserRepository],
 })
 export class StoryModule {}
