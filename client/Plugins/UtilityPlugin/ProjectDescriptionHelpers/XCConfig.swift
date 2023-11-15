25e0b31503ab0c82be572d1e239fe6f5609c20db
hogumachu <castle_jun@daum.net>
2023-11-15 16:11:37 +0900

    package: Secret Key 추가

---
+++ client/Plugins/UtilityPlugin/ProjectDescriptionHelpers/XCConfig.swift
@@ -0,0 +1,8 @@
+//
+//  XCConfig.swift
+//  UtilityPlugin
+//
+//  Created by 홍성준 on 11/15/23.
+//
+
+import Foundation
---
+++ client/Targets/Core/CoreKit/Sources/SecretUtil/Secret.swift
@@ -0,0 +1,9 @@
+//
+//  Secret.swift
+//  CoreKit
+//
+//  Created by 홍성준 on 11/15/23.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import Foundation
