96e434410597ea8d5e9a7f571d82f31e2a88ac4a
Geomgi <cgg7777@naver.com>
2023-11-15 20:23:37 +0900

    test: 검색어 자동완성 기능 테스트

--- server/src/search/search.service.spec.ts
+++ server/src/search/search.service.spec.ts
@@ -1,27 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SearchService } from './search.service';
+import { JasoTrie } from './trie/trie';
 
 describe('SearchService', () => {
   let service: SearchService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SearchService],
+      providers: [SearchService, JasoTrie],
     }).compile();
 
     service = module.get<SearchService>(SearchService);
   });
 
   it('Graphme Separation Test', () => {
-    expect(service.graphemeSeparation('안녕하세요')).toBe('ㅇㅏㄴㄴㅕㅇㅎㅏㅅㅔㅇㅛ');
+    expect(service.graphemeSeparation('안녕하세요')).toStrictEqual(['ㅇ', 'ㅏ', 'ㄴ', 'ㄴ', 'ㅕ', 'ㅇ', 'ㅎ', 'ㅏ', 'ㅅ', 'ㅔ', 'ㅇ', 'ㅛ']);
   });
   it('Graphme Separation Test Including blank', () => {
-    expect(service.graphemeSeparation('안녕 하세요')).toBe('ㅇㅏㄴㄴㅕㅇ ㅎㅏㅅㅔㅇㅛ');
+    expect(service.graphemeSeparation('안녕 하세요')).toStrictEqual(['ㅇ', 'ㅏ', 'ㄴ', 'ㄴ', 'ㅕ', 'ㅇ', ' ', 'ㅎ', 'ㅏ', 'ㅅ', 'ㅔ', 'ㅇ', 'ㅛ']);
   });
   it('Graphme Combination Test', () => {
-    expect(service.graphemeCombination('ㅇㅏㄴㄴㅕㅇㅎㅏㅅㅔㅇㅛ')).toBe('안녕하세요');
+    expect(service.graphemeCombination(['ㅇ', 'ㅏ', 'ㄴ', 'ㄴ', 'ㅕ', 'ㅇ', 'ㅎ', 'ㅏ', 'ㅅ', 'ㅔ', 'ㅇ', 'ㅛ'])).toBe('안녕하세요');
   });
   it('Graphme Combination Test Including blank', () => {
-    expect(service.graphemeCombination('ㅇㅏㄴㄴㅕㅇ ㅎㅏㅅㅔㅇㅛ')).toBe('안녕 하세요');
+    expect(service.graphemeCombination(['ㅇ', 'ㅏ', 'ㄴ', 'ㄴ', 'ㅕ', 'ㅇ', ' ', 'ㅎ', 'ㅏ', 'ㅅ', 'ㅔ', 'ㅇ', 'ㅛ'])).toBe('안녕 하세요');
+  });
+  it('Search Automated Complete Test', () => {
+    service.insertTree(service.graphemeSeparation('안녕하세요'));
+    service.insertTree(service.graphemeSeparation('안녕하십니까'));
+    service.insertTree(service.graphemeSeparation('안녕'));
+
+    const recommendedWords = service.searchTree(service.graphemeSeparation('안녕하'));
+    expect(recommendedWords).toEqual(['안녕하세요', '안녕하십니까']);
   });
 });
