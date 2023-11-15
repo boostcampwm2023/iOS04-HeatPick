2db65e5138a5f78a5d392418d2c839e7665690a8
Geomgi <cgg7777@naver.com>
2023-11-15 20:23:20 +0900

    feat: 검색어 자동완성 기능 구현

--- server/src/search/search.service.ts
+++ server/src/search/search.service.ts
@@ -1,17 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import * as Hangul from 'hangul-js';
+import { JasoTrie } from './trie/trie';
 
 @Injectable()
 export class SearchService {
+  constructor(private jasoTrie: JasoTrie) {}
+  // 구현중
   async recommendSearch(): Promise<any> {
     return 1;
   }
 
-  graphemeSeparation(text: string): string {
-    return Hangul.disassemble(text).join('');
+  insertTree(separatedStatement: string[]) {
+    this.jasoTrie.insert(separatedStatement);
   }
 
-  graphemeCombination(separatedStatement: string): string {
-    return Hangul.assemble(separatedStatement.split(''));
+  searchTree(separatedStatement: string[]) {
+    const recommendedWords = this.jasoTrie.search(separatedStatement);
+    return recommendedWords.map((word) => this.graphemeCombination(word));
+  }
+
+  graphemeSeparation(text: string): string[] {
+    return Hangul.disassemble(text);
+  }
+
+  graphemeCombination(separatedStatement: string[]): string {
+    return Hangul.assemble(separatedStatement);
   }
 }
