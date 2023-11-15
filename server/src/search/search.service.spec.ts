d344d478214e456ef0a2b96e1146a540ba2ff0f3
Geomgi <cgg7777@naver.com>
2023-11-15 19:28:11 +0900

    test: SearchService unit test 작성

--- server/src/search/search.service.spec.ts
+++ server/src/search/search.service.spec.ts
@@ -15,4 +15,13 @@ describe('SearchService', () => {
   it('Graphme Separation Test', () => {
     expect(service.graphemeSeparation('안녕하세요')).toBe('ㅇㅏㄴㄴㅕㅇㅎㅏㅅㅔㅇㅛ');
   });
+  it('Graphme Separation Test Including blank', () => {
+    expect(service.graphemeSeparation('안녕 하세요')).toBe('ㅇㅏㄴㄴㅕㅇ ㅎㅏㅅㅔㅇㅛ');
+  });
+  it('Graphme Combination Test', () => {
+    expect(service.graphemeCombination('ㅇㅏㄴㄴㅕㅇㅎㅏㅅㅔㅇㅛ')).toBe('안녕하세요');
+  });
+  it('Graphme Combination Test Including blank', () => {
+    expect(service.graphemeCombination('ㅇㅏㄴㄴㅕㅇ ㅎㅏㅅㅔㅇㅛ')).toBe('안녕 하세요');
+  });
 });
