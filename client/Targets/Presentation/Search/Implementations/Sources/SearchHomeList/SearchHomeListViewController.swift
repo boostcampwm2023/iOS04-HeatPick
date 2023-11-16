e0166054c8d16e9dd0aae1b6c44599e1543d266e
junbok97 <junbok97@gmail.com>
2023-11-16 16:38:20 +0900

    feat: SearchHomeList UI 추가

--- client/Targets/Presentation/Search/Implementations/Sources/SearchHomeList/SearchHomeListViewController.swift
+++ client/Targets/Presentation/Search/Implementations/Sources/SearchHomeList/SearchHomeListViewController.swift
@@ -6,65 +6,106 @@
 //  Copyright © 2023 codesquad. All rights reserved.
 //
 
-import ModernRIBs
 import UIKit
+import DesignKit
+import ModernRIBs
+
 
 protocol SearchHomeListPresentableListener: AnyObject {
-    
+    func didTapitem(model: SearchHomeListCellModel)
 }
 
 final class SearchHomeListViewController: UIViewController, SearchHomeListPresentable, SearchHomeListViewControllable {
     
     weak var listener: SearchHomeListPresentableListener?
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        setupSheet()
+    private var models: [SearchHomeListCellModel] = []
+    
+    private enum Constant {
+        static let topOffset: CGFloat = 20
     }
     
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setupSheet()
-    }
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(SearchHomeListCell.self)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private let locationLabel: UILabel = {
+       let label = UILabel()
+        label.font = .bodyBold
+        label.text = "현재 위치"
+        label.textAlignment = .center
+        label.textColor = .hpBlack
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupViews()
     }
+    
+    func updateLocation(_ location: String) {
+        locationLabel.text = location
+    }
+    
+    func setup(models: [SearchHomeListCellModel]) {
+        self.models = models
+        tableView.reloadData()
+    }
+    
+    func append(models: [SearchHomeListCellModel]) {
+        self.models.append(contentsOf: models)
+        tableView.reloadData()
+    }
+    
 }
 
 private extension SearchHomeListViewController {
-    func setupSheet() {
-        modalPresentationStyle = .custom
-        transitioningDelegate = self
-        isModalInPresentation = true
+
+    func setupViews() {
+        view.backgroundColor = .hpWhite
+        [locationLabel, tableView].forEach { view.addSubview($0) }
+        NSLayoutConstraint.activate([
+            locationLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Constant.topOffset),
+            locationLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            locationLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: Constant.topOffset),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Constants.leadingOffset),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Constants.traillingOffset),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
     
-    func setupViews() {
-        view.backgroundColor = .hpGray4
-    }
 }
 
-extension SearchHomeListViewController: UIViewControllerTransitioningDelegate {
-    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-        let sheetPresentationController = SearchHomeListSheetPresentationController(presentedViewController: presented, presenting: source)
-        sheetPresentationController.detents = [
-            .small(),
-            .medium(),
-            .large(),
-        ]
-        
-        sheetPresentationController.selectedDetentIdentifier = .small
-        sheetPresentationController.largestUndimmedDetentIdentifier = .large
-        
-        sheetPresentationController.prefersGrabberVisible = true
-        sheetPresentationController.prefersScrollingExpandsWhenScrolledToEdge = false
-        sheetPresentationController.widthFollowsPreferredContentSizeWhenEdgeAttached = true
-        
-        return sheetPresentationController
+extension SearchHomeListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        models.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let model = models[safe: indexPath.row] else {
+            return .init()
+        }
+        let cell = tableView.dequeue(SearchHomeListCell.self, for: indexPath)
+        cell.setup(model: model)
+        return cell
+    }
+    
 }
 
-
-
+extension SearchHomeListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let model = models[safe: indexPath.row] else { return }
+        listener?.didTapitem(model: model)
+    }
+    
+}
