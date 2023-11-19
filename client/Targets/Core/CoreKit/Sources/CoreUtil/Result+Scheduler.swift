1c51f0f3657017379a88bc9fdc440c478b5f2c40
hogumachu <castle_jun@daum.net>
2023-11-19 18:17:35 +0900

    feat: Result sugar 추가

--- client/Targets/Core/CoreKit/Sources/CoreUtil/Result+Scheduler.swift
+++ client/Targets/Core/CoreKit/Sources/CoreUtil/Result+Scheduler.swift
@@ -1,5 +1,5 @@
 //
-//  Result+.swift
+//  Result+Scheduler.swift
 //  CoreKit
 //
 //  Created by 홍성준 on 11/19/23.
@@ -9,6 +9,7 @@
 import Foundation
 
 public enum ResultScheduler {
+    
     case main
     case global
     case current
@@ -29,6 +30,7 @@ public enum ResultScheduler {
             handler()
         }
     }
+    
 }
 
 public extension Result {
@@ -51,6 +53,25 @@ public extension Result {
     }
     
     @discardableResult
+    func onSuccess<T: AnyObject>(
+        on scheduler: ResultScheduler = .current,
+        with unretainObject: T,
+        _ execute: @escaping (T, Success) -> Void
+    ) -> Self {
+        switch self {
+        case .success(let value):
+            scheduler.exeucte { [weak unretainObject] in
+                guard let object = unretainObject else { return }
+                execute(object, value)
+            }
+            return self
+            
+        default:
+            return self
+        }
+    }
+    
+    @discardableResult
     func onFailure(
         on scheduler: ResultScheduler = .current,
         _ execute: @escaping (Failure) -> Void
@@ -59,6 +80,25 @@ public extension Result {
         case .failure(let error):
             scheduler.exeucte {
                 execute(error)
+            }
+            return self
+            
+        default:
+            return self
+        }
+    }
+    
+    @discardableResult
+    func onFailure<T: AnyObject>(
+        on scheduler: ResultScheduler = .current,
+        with unretainObject: T,
+        _ execute: @escaping (T, Failure) -> Void
+    ) -> Self {
+        switch self {
+        case .failure(let error):
+            scheduler.exeucte { [weak unretainObject] in
+                guard let object = unretainObject else { return }
+                execute(object, error)
             }
             return self
             
