89d00e73c07a4a29cb4e6e14f6d9d4d252093361
junbok97 <junbok97@gmail.com>
2023-11-20 17:47:51 +0900

    chore: SearchResultRouterComponent추가

--- client/Targets/Presentation/Search/Implementations/Sources/SearchResult/SearchResultRouter.swift
+++ client/Targets/Presentation/Search/Implementations/Sources/SearchResult/SearchResultRouter.swift
@@ -8,22 +8,47 @@
 
 import ModernRIBs
 
-protocol SearchResultInteractable: Interactable {
+protocol SearchResultInteractable: Interactable,
+                                   BeginEditingTextDashboardListener,
+                                   EditingTextDashboardListener,
+                                    EndEditingTextDashboardListener {
     var router: SearchResultRouting? { get set }
     var listener: SearchResultListener? { get set }
 }
 
-protocol SearchResultViewControllable: ViewControllable { }
+protocol SearchResultViewControllable: ViewControllable {
+    func attachDashboard(_ viewControllable: ViewControllable)
+    func detachDashboard(_ viewControllable: ViewControllable)
+}
+
+protocol SearchResultRouterDependency {
+    var beginEditingTextDashboardBuilder: BeginEditingTextDashboardBuildable { get }
+    var editingTextDashboardBuilder: EditingTextDashboardBuildable { get }
+    var endEditingTextDashboardBuilder: EndEditingTextDashboardBuildable { get }
+}
 
 final class SearchResultRouter: ViewableRouter<SearchResultInteractable, SearchResultViewControllable>, SearchResultRouting {
-
-    override init(
+    
+    private let beginEditingTextDashboardBuilder: BeginEditingTextDashboardBuildable
+    private var beginEditingTextDasboardRouter: Routing?
+    
+    private let editingTextDashboardBuilder: EditingTextDashboardBuildable
+    private var editingTextDashboardRouter: Routing?
+    
+    private let endEditingTextDashboardBuilder: EndEditingTextDashboardBuildable
+    private var endEditingTextDashboardRouter: Routing?
+    
+    init(
         interactor: SearchResultInteractable,
-        viewController: SearchResultViewControllable
+        viewController: SearchResultViewControllable,
+        dependency: SearchResultRouterDependency
     ) {
+        self.beginEditingTextDashboardBuilder = dependency.beginEditingTextDashboardBuilder
+        self.editingTextDashboardBuilder = dependency.editingTextDashboardBuilder
+        self.endEditingTextDashboardBuilder = dependency.endEditingTextDashboardBuilder
         super.init(interactor: interactor, viewController: viewController)
         interactor.router = self
     }
     
-
+    
 }
