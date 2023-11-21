d56994653fcb933f899ae24232a346066875b759
jungminlim <jungmin.lim.cs@gmail.com>
2023-11-21 09:52:16 +0900

    feat: Story에 필요한 entity 추가

---
+++ client/Targets/Domain/Entities/Sources/Story/Story.swift
@@ -0,0 +1,21 @@
+//
+//  Story.swift
+//  DomainEntities
+//
+//  Created by jungmin lim on 11/20/23.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import Foundation
+
+public struct Story {
+    
+    public let id: Int
+    public let storyContent: StoryContent
+    
+    public init(id: Int, storyContent: StoryContent) {
+        self.id = id
+        self.storyContent = storyContent
+    }
+    
+}
