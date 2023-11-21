2c0e8401e11e02769aef388fe23cdae8c1f98c1a
Geomgi <cgg7777@naver.com>
2023-11-21 14:07:12 +0900

    feat: 새로운 대표 뱃지 설정 스웨거 작성

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -9,13 +9,15 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('badge')
-  @ApiOperation({ summary: 'Add a new badge to a user' })
-  @ApiResponse({ status: 201, description: 'Badge가 성공적으로 추가되었습니다.' })
+  @ApiOperation({ summary: '유제 객체에 새로운 뱃지를 추가합니다.' })
+  @ApiResponse({ status: 200, description: 'Badge가 성공적으로 추가되었습니다.' })
   async addBadge(@Body() addBadgeDto: AddBadgeDto) {
     return this.userService.addNewBadge(addBadgeDto);
   }
 
   @Put('badge')
+  @ApiOperation({ summary: '유저 객체의 대표 뱃지를 설정합니다.' })
+  @ApiResponse({ status: 200, description: '대표 뱃지가 성공적으로 변경되었습니다.' })
   async setRepresentatvieBadge(@Body() setBadgeDto: AddBadgeDto) {
     return this.userService.setRepresentatvieBadge(setBadgeDto);
   }
