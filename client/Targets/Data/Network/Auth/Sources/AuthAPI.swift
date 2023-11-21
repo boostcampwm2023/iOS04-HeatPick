8d030561821df15beb26ce54452a65b38a6dab0c
hogumachu <castle_jun@daum.net>
2023-11-21 11:50:03 +0900

    feat: Authorized Header 추가

--- client/Targets/Data/Network/Auth/Sources/AuthAPI.swift
+++ client/Targets/Data/Network/Auth/Sources/AuthAPI.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import BaseAPI
 import NetworkAPIKit
 
 public enum AuthAPI {
---
+++ client/Targets/Data/Network/Base/Sources/NetworkHeader+Authorized.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkHeader+Authorized.swift
+//  BaseAPI
+//
+//  Created by 홍성준 on 11/21/23.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import Foundation
+import CoreKit
+import NetworkAPIKit
+
+public extension NetworkHeader {
+    
+    static var token: String? {
+        guard let data = SecretManager.read(type: .accessToken) else {
+            return nil
+        }
+        return String(data: data, encoding: .utf8)
+    }
+    
+    static var authorized: NetworkHeader {
+        guard let token = token else { return .default }
+        return .default.accessToken(token)
+    }
+    
+}
