93619edccc6569fec9b04b7eea7965fd0cba5363
hogumachu <castle_jun@daum.net>
2023-11-21 13:29:54 +0900

    feat: HomeUseCase 추가

---
+++ client/Targets/Domain/UseCases/Sources/HomeUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  HomeUseCase.swift
+//  DomainUseCases
+//
+//  Created by 홍성준 on 11/21/23.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import Foundation
+import CoreKit
+import DomainEntities
+import DomainInterfaces
+
+public final class HomeUseCase: HomeUseCaseInterface {
+    
+    private let repository: HomeRepositoryInterface
+    
+    public init(repository: HomeRepositoryInterface) {
+        self.repository = repository
+    }
+    
+    public func fetchHotPlace() async -> Result<[RecommendStory], Error> {
+        return await repository.fetchHotPlace()
+    }
+    
+}
