ddf83782d9ab5a68d7bbcd32ab492967c681f8ee
Geomgi <cgg7777@naver.com>
2023-11-22 19:48:27 +0900

    fix: dto 자료형 에러 수정

--- server/src/place/dto/location.dto.ts
+++ server/src/place/dto/location.dto.ts
@@ -1,12 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform, TransformFnParams } from 'class-transformer';
 import { IsNumber } from 'class-validator';
 
 export class LocationDTO {
   @ApiProperty({ example: '40.0', description: '위치 정보의 위도에 해당합니다.' })
+  @Transform(({ value }: TransformFnParams) => parseFloat(value))
   @IsNumber()
   latitude: number;
 
   @ApiProperty({ example: '50.5', description: '위치 정보의 경도에 해당합니다.' })
+  @Transform(({ value }: TransformFnParams) => parseFloat(value))
   @IsNumber()
   longitude: number;
 }
