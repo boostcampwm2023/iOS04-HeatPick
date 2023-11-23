1ffac7c99198feef6ef05fd5a14ff90d0b3cf280
junbok97 <junbok97@gmail.com>
2023-11-23 14:28:30 +0900

    chore: SearchBefore 하위 리블렛 attach

--- client/Targets/Presentation/Search/Implementations/Sources/SearchResult/SearchBeforeDashboard/SearchBeforeDashboardRouter.swift
+++ client/Targets/Presentation/Search/Implementations/Sources/SearchResult/SearchBeforeDashboard/SearchBeforeDashboardRouter.swift
@@ -8,17 +8,70 @@
 
 import ModernRIBs
 
-protocol SearchBeforeDashboardInteractable: Interactable {
+protocol SearchBeforeDashboardInteractable: Interactable,
+                                            SearchBeforeRecentSearchesDashboardListener,
+                                            SearchBeforeCategoryDashboardListener {
     var router: SearchBeforeDashboardRouting? { get set }
     var listener: SearchBeforeDashboardListener? { get set }
 }
 
-protocol SearchBeforeDashboardViewControllable: ViewControllable { }
+protocol SearchBeforeDashboardViewControllable: ViewControllable {
+    func appendDashboard(_ viewControllable: ViewControllable)
+    func removeDashboard(_ viewControllable: ViewControllable)
+}
+
+protocol SearchBeforeDashboardRouterDependency {
+    var searchBeforeRecentSearchesDashboardBuilder: SearchBeforeRecentSearchesDashboardBuildable { get }
+    var searchBeforeCategoryDashboardBuilder: SearchBeforeCategoryDashboardBuildable { get }
+}
 
 final class SearchBeforeDashboardRouter: ViewableRouter<SearchBeforeDashboardInteractable, SearchBeforeDashboardViewControllable>, SearchBeforeDashboardRouting {
-
-    override init(interactor: SearchBeforeDashboardInteractable, viewController: SearchBeforeDashboardViewControllable) {
+    
+    private let searchBeforeRecentSearchesDashboardBuilder: SearchBeforeRecentSearchesDashboardBuildable
+    private var searchBeforeRecentSearchsDashboardRouter: SearchBeforeRecentSearchesDashboardRouting?
+    
+    private let searchBeforeCategoryDashboardBuilder: SearchBeforeCategoryDashboardBuildable
+    private var searchBeforeCategoryDashboardRouter: SearchBeforeCategoryDashboardRouting?
+    
+    init(
+        interactor: SearchBeforeDashboardInteractable,
+        viewController: SearchBeforeDashboardViewControllable,
+        dependency: SearchBeforeDashboardRouterDependency
+    ) {
+        self.searchBeforeRecentSearchesDashboardBuilder = dependency.searchBeforeRecentSearchesDashboardBuilder
+        self.searchBeforeCategoryDashboardBuilder = dependency.searchBeforeCategoryDashboardBuilder
         super.init(interactor: interactor, viewController: viewController)
         interactor.router = self
     }
+    
+    func attachSearchBeforeRecentSearchesDashboard() {
+        guard searchBeforeRecentSearchsDashboardRouter == nil else { return }
+        let router = searchBeforeRecentSearchesDashboardBuilder.build(withListener: interactor)
+        attachChild(router)
+        searchBeforeRecentSearchsDashboardRouter = router
+        viewController.appendDashboard(router.viewControllable)
+    }
+    
+    func detachSearchBeforeRecentSearchesDashboard() {
+        guard let router = searchBeforeRecentSearchsDashboardRouter else { return }
+        viewController.removeDashboard(router.viewControllable)
+        detachChild(router)
+        searchBeforeRecentSearchsDashboardRouter = nil
+    }
+    
+    func attachSearchBeforeCategoryDashboard() {
+        guard searchBeforeCategoryDashboardRouter == nil else { return }
+        let router = searchBeforeCategoryDashboardBuilder.build(withListener: interactor)
+        attachChild(router)
+        searchBeforeCategoryDashboardRouter = router
+        viewController.appendDashboard(router.viewControllable)
+    }
+    
+    func detachSearchBeforeCategoryDashboard() {
+        guard let router = searchBeforeCategoryDashboardRouter else { return }
+        viewController.removeDashboard(router.viewControllable)
+        detachChild(router)
+        searchBeforeCategoryDashboardRouter = nil
+    }
+    
 }
--- client/Targets/Presentation/Search/Implementations/Sources/SearchResult/SearchBeforeDashboard/SearchBeforeDashboardViewController.swift
+++ client/Targets/Presentation/Search/Implementations/Sources/SearchResult/SearchBeforeDashboard/SearchBeforeDashboardViewController.swift
@@ -6,210 +6,79 @@
 //  Copyright © 2023 codesquad. All rights reserved.
 //
 
-import ModernRIBs
 import UIKit
+import DesignKit
+import ModernRIBs
 
-protocol SearchBeforeDashboardPresentableListener: AnyObject {
-    func didTapCategoryItem()
-    func didTapRecentSearchTextItem()
-}
+
+protocol SearchBeforeDashboardPresentableListener: AnyObject { }
 
 final class SearchBeforeDashboardViewController: UIViewController, SearchBeforeDashboardPresentable, SearchBeforeDashboardViewControllable {
 
     weak var listener: SearchBeforeDashboardPresentableListener?
     
     private enum Constant {
-        static let inset: CGFloat = 20
-        enum Section {
-            static let recentSearchText = "최근 검색어"
-            static let categoryTitle = "카테고리"
-        }
+        static let spacing: CGFloat = 20
     }
     
-    private var categoryModels: [CategoryModel] = []
-    private var recentSearchTextModels: [ReceentSearchTextModel] = []
-    
-    private lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout())
-        collectionView.register(SearchBeforeRecentSearchTextCell.self)
-        collectionView.register(SearchBeforeCategoryCell.self)
-        collectionView.registerHeader(SearchBeforeHeaderView.self)
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        return collectionView
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.contentInset = .zero
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
     }()
     
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = Constant.spacing
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
     }
     
-    func setup(categoryModels: [CategoryModel]) {
-        self.categoryModels = categoryModels
-        collectionView.reloadData()
+    func appendDashboard(_ viewControllable: ViewControllable) {
+        let viewController = viewControllable.uiviewController
+        addChild(viewController)
+        stackView.addArrangedSubview(viewController.view)
+        viewController.didMove(toParent: self)
     }
     
-    func append(categoryModels: [CategoryModel]) {
-        self.categoryModels.append(contentsOf: categoryModels)
-        collectionView.reloadData()
+    func removeDashboard(_ viewControllable: ViewControllable) {
+        let viewController = viewControllable.uiviewController
+        stackView.removeArrangedSubview(viewController.view)
+        viewController.removeFromParent()
     }
     
-    func setup(recentSearchTextModels: [ReceentSearchTextModel]) {
-        self.recentSearchTextModels = recentSearchTextModels
-        collectionView.reloadData()
-    }
-    
-    func append(recentSearchTextModels: [ReceentSearchTextModel]) {
-        self.recentSearchTextModels.append(contentsOf: recentSearchTextModels)
-        collectionView.reloadData()
-    }
 }
-
 
 private extension SearchBeforeDashboardViewController {
     
     func setupViews() {
-        view.addSubview(collectionView)
+        view.backgroundColor = .hpWhite
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            scrollView.frameLayoutGuide.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.frameLayoutGuide.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.frameLayoutGuide.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.frameLayoutGuide.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            stackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor)
         ])
     }
     
-    func makeCollectionViewLayout() -> UICollectionViewCompositionalLayout {
-        
-        let layout = UICollectionViewCompositionalLayout { [weak self] section, collectionLayoutEnvironment -> NSCollectionLayoutSection? in
-            guard let self = self,
-                  let section = Section(rawValue: section) else { return nil }
-            switch section {
-            case .recentSearchText:
-                return self.makeRecentSearchSection()
-            case .category:
-                return self.makeCategorySection()
-            }
-        }
-
-        return layout
-    }
-    
-    func makeRecentSearchSection() -> NSCollectionLayoutSection {
-        let size = NSCollectionLayoutSize(widthDimension: .estimated(100), heightDimension: .estimated(30))
-        let item = NSCollectionLayoutItem(layoutSize: size)
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitems: [item])
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .groupPaging
-        section.interGroupSpacing = 10
-        section.boundarySupplementaryItems = [
-                NSCollectionLayoutBoundarySupplementaryItem(
-                    layoutSize: .init(
-                        widthDimension: .fractionalWidth(1),
-                        heightDimension: .estimated(50)
-                    ),
-                    elementKind: UICollectionView.elementKindSectionHeader,
-                    alignment: .top
-                )
-            ]
-
-        
-        return section
-    }
-    
-    func makeCategorySection() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
-        group.interItemSpacing = .fixed(10)
-        group.contentInsets = .init(top: 0, leading: 20, bottom: 0, trailing: 20)
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .continuous
-        
-        section.boundarySupplementaryItems = [
-            NSCollectionLayoutBoundarySupplementaryItem(
-                layoutSize: .init(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .estimated(50)
-                ),
-                elementKind: UICollectionView.elementKindSectionHeader,
-                alignment: .top
-            )
-        ]
-        
-        return section
-    }
-    
 }
-
-// TODO: Snapshow으로 변경 예정
-extension SearchBeforeDashboardViewController: UICollectionViewDataSource {
-    
-    private enum Section: Int, CaseIterable {
-        case recentSearchText
-        case category
-        
-        var title: String {
-            switch self {
-            case .recentSearchText:
-                return Constant.Section.recentSearchText
-            case .category:
-                return Constant.Section.categoryTitle
-            }
-        }
-    }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        Section.allCases.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        guard let section = Section(rawValue: section) else { return 0 }
-        switch section {
-        case .recentSearchText:
-            return recentSearchTextModels.count
-        case .category:
-            return categoryModels.count
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let section = Section(rawValue: indexPath.section) else { return .init() }
-
-        switch section {
-        case .recentSearchText:
-            guard let model = recentSearchTextModels[safe: indexPath.row] else { return .init() }
-            let cell = collectionView.dequeue(SearchBeforeRecentSearchTextCell.self, for: indexPath)
-            cell.setup(model)
-            return cell
-        case .category:
-            guard let model = categoryModels[safe: indexPath.row] else { return .init() }
-            let cell = collectionView.dequeue(SearchBeforeCategoryCell.self, for: indexPath)
-            cell.setup(model)
-            return cell
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        if kind == UICollectionView.elementKindSectionHeader,
-           let section = Section(rawValue: indexPath.section) {
-            let headerView = collectionView.dequeueHeader(SearchBeforeHeaderView.self, for: indexPath)
-            headerView.setupTitle(section.title)
-            return headerView
-        }
-        
-        return .init()
-    }
-    
-}
-
-extension SearchBeforeDashboardViewController: UICollectionViewDelegate {
-    
-}
-
-
