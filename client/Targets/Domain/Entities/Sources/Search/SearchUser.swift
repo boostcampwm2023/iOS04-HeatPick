29eeba39ca01c13259d76b52ed495c849ed89028
junbok97 <junbok97@gmail.com>
2023-11-28 17:04:38 +0900

    chore: Search Model 수정

--- client/Targets/Domain/Entities/Sources/Search/SearchUser.swift
+++ client/Targets/Domain/Entities/Sources/Search/SearchUser.swift
@@ -12,34 +12,12 @@ public struct SearchUser {
     
     public let userId: Int
     public let username: String
-    public let oauthId: String
-    public let temperature: Int
-    public let createAt: String
-    public let deletedAt: String
-    public let recentActive: String
-    public let following: [String]
-    public let followers: [String]
+    public let profileUrl: String
     
-    public init(
-        userId: Int,
-        username: String,
-        oauthId: String,
-        temperature: Int,
-        createAt: String,
-        deletedAt: String,
-        recentActive: String,
-        following: [String],
-        followers: [String]
-    ) {
+    public init(userId: Int, username: String, profileUrl: String) {
         self.userId = userId
         self.username = username
-        self.oauthId = oauthId
-        self.temperature = temperature
-        self.createAt = createAt
-        self.deletedAt = deletedAt
-        self.recentActive = recentActive
-        self.following = following
-        self.followers = followers
+        self.profileUrl = profileUrl
     }
     
 }
