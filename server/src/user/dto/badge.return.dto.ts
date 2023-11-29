73c75918c9190dc0cdec6cece948fcc0168517af
Geomgi <cgg7777@naver.com>
2023-11-29 11:34:02 +0900

    feat: 뱃지 리턴 dto 정의

---
+++ server/src/user/dto/badge.return.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class BadgeReturnDto {
+  @ApiProperty({ description: '뱃지의 아이디입니다.' })
+  badgeId: number;
+
+  @ApiProperty({ description: '뱃지의 이름입니다.' })
+  badgeName: string;
+
+  @ApiProperty({ description: '뱃지의 이모지입니다.' })
+  emoji: string;
+
+  @ApiProperty({ description: '뱃지의 설명입니다.' })
+  description: string;
+}
