2ecdc67db09d3e86c012f3970db2ce90f5163cc9
junbok97 <junbok97@gmail.com>
2023-11-29 14:02:56 +0900

    feat: SearchInterface 추가

--- client/Targets/Presentation/Search/Interfaces/Sources/SearchInterface.swift
+++ client/Targets/Presentation/Search/Interfaces/Sources/SearchInterface.swift
@@ -1,1 +1,9 @@
-//Search.swift
+//
+//  SearchInterface.swift
+//  SearchInterfaces
+//
+//  Created by 이준복 on 11/29/23.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import Foundation
