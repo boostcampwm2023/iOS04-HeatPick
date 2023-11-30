1d4b1fffe9b70a4975729c4ed65f02067f069bcc
hogumachu <castle_jun@daum.net>
2023-11-30 16:11:48 +0900

    chore: API Response 수정

--- client/Targets/Data/Network/Home/Sources/DTO/RecommendLocationResponseDTO.swift
+++ client/Targets/Data/Network/Home/Sources/DTO/RecommendLocationResponseDTO.swift
@@ -15,7 +15,7 @@ public struct RecommendLocationResponseDTO: Decodable {
         public let storyId: Int
         public let title: String
         public let content: String
-        public let storyImage: String
+        public let storyImage: String?
         public let likeCount: Int
         public let commentCount: Int
         public let latitude: Double
@@ -42,7 +42,7 @@ public extension RecommendLocationResponseDTO {
                 id: $0.storyId,
                 title: $0.title,
                 content: $0.content,
-                imageURL: $0.storyImage,
+                imageURL: $0.storyImage ?? "",
                 likes: $0.likeCount,
                 comments: $0.commentCount,
                 lat: $0.latitude,
