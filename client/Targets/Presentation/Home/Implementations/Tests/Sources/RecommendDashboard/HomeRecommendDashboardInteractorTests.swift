32c055a16f03fad593c0d067d72617cef24b5632
hogumachu <castle_jun@daum.net>
2023-12-04 23:45:59 +0900

    test: HomeRecommendDashboardMock 추가

---
+++ client/Targets/Presentation/Home/Implementations/Tests/Sources/RecommendDashboard/HomeRecommendDashboardInteractorTests.swift
@@ -0,0 +1,24 @@
+//
+//  HomeRecommendDashboardInteractorTests.swift
+//  HomeImplementationsTests
+//
+//  Created by 홍성준 on 12/4/23.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+@testable import HomeImplementations
+import XCTest
+import ModernRIBs
+import PresentationTestUtil
+
+final class HomeRecommendDashboardInteractorTests: XCTestCase {
+    
+    private var sut: HomeRecommendDashboardInteractor!
+    
+    override func setUp() {
+        super.setUp()
+        
+        
+    }
+    
+}
