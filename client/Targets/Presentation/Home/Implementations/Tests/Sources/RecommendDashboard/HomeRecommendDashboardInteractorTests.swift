bb288fa173774ece4e5524857dbe38b6351979df
hogumachu <castle_jun@daum.net>
2023-12-05 00:09:43 +0900

    test: HomeRecommendDashboardInteractor 테스트 추가

--- client/Targets/Presentation/Home/Implementations/Tests/Sources/RecommendDashboard/HomeRecommendDashboardInteractorTests.swift
+++ client/Targets/Presentation/Home/Implementations/Tests/Sources/RecommendDashboard/HomeRecommendDashboardInteractorTests.swift
@@ -8,17 +8,66 @@
 
 @testable import HomeImplementations
 import XCTest
+import Combine
 import ModernRIBs
+import DomainEntities
 import PresentationTestUtil
 
 final class HomeRecommendDashboardInteractorTests: XCTestCase {
     
     private var sut: HomeRecommendDashboardInteractor!
+    private var presenter: HomeRecommendDashboardPresentableMock!
+    private var dependency: HomeRecommendDashboardInteractorDependencyMock!
+    private var listener: HomeRecommendDashboardListenerMock!
+    private var router: HomeRecommendDashboardRoutingMock!
     
     override func setUp() {
         super.setUp()
         
+        presenter = HomeRecommendDashboardPresentableMock()
+        dependency = HomeRecommendDashboardInteractorDependencyMock(hasMoreRecommendPlace: true)
+        listener = HomeRecommendDashboardListenerMock()
+        router = HomeRecommendDashboardRoutingMock(interactable: Interactor(), viewControllable: ViewControllableMock())
         
+        sut = HomeRecommendDashboardInteractor(
+            presenter: presenter,
+            dependency: dependency
+        )
+        sut.listener = listener
+        sut.router = router
+    }
+    
+    func test_화면이_나타날때마다_현재_위치_업데이트_요청을_한다() {
+        // when
+        sut.didAppear()
+        
+        // then
+        XCTAssertEqual(dependency.recommendUseCaseMock.updateCurrentLocationCallCount, 1)
+    }
+    
+    func test_모두보기는_위치정보가_없으면_호출되지_않는다() {
+        // given
+        dependency.recommendUseCaseMock.location = nil
+        
+        // when
+        sut.didTapSeeAll()
+        
+        // then
+        XCTAssertEqual(listener.recommendDashboardDidTapSeeAllCallCount, 0)
+    }
+    
+    func test_모두보기는_위치정보가_있으면_호출된다() {
+        // given
+        let location = LocationCoordinate(lat: 10, lng: 20)
+        dependency.recommendUseCaseMock.location = location
+        
+        // when
+        sut.didTapSeeAll()
+        
+        // then
+        XCTAssertEqual(listener.recommendDashboardDidTapSeeAllCallCount, 1)
+        XCTAssertEqual(listener.recommendDashboardDidTapSeeAllLocation?.lat, location.lat)
+        XCTAssertEqual(listener.recommendDashboardDidTapSeeAllLocation?.lng, location.lng)
     }
     
 }
