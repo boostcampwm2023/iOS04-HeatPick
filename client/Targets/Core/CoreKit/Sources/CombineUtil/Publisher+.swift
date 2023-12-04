c0954ad0220be0a23e893f0d4f8514190dc6a10e
hogumachu <castle_jun@daum.net>
2023-12-04 12:38:27 +0900

    feat: combine unretain 추가

---
+++ client/Targets/Core/CoreKit/Sources/CombineUtil/Publisher+.swift
@@ -0,0 +1,24 @@
+//
+//  Combine+.swift
+//  CoreKit
+//
+//  Created by 홍성준 on 12/4/23.
+//  Copyright © 2023 codesquad. All rights reserved.
+//
+
+import Combine
+
+public extension Publisher {
+    
+    func with<O: AnyObject>(_ object: O) -> Publishers.CompactMap<Self, (O, Self.Output)> {
+        return compactMap { [weak object] output in
+            guard let object else { return nil }
+            return (object, output)
+        }
+    }
+    
+    func sinkWith<O: AnyObject>(_ object: O) -> Publishers.CompactMap<Self, (O, Self.Output)> {
+        return with(object)
+    }
+    
+}
